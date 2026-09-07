25ef6f4c19b1b7e2a54199ee134718c2422e07be
airi <airi.hisa@icloud.com>
2026-09-07 22:11:51 +0900

    Update Tabbar.tsx

--- src/App/Tabbar.tsx
+++ src/App/Tabbar.tsx
@@ -10,8 +10,8 @@ const Content = () => {
   return (
     <div className="tabbar">
       <ul>
-        <li><Link to="/"><div className="icon"><FaHome /></div><div className="text">ホーム</div></Link></li>
-        <li><Link to="/list"><div className="icon"><FaList /></div><div className="text">一覧</div></Link></li>
+        <li><Link to="/"><div className="icon"><FaHome /></div><div className="text">マップ</div></Link></li>
+        <li><Link to="/list"><div className="icon"><FaList /></div><div className="text">リスト</div></Link></li>
         {/* カテゴリの行を削除しました */}
         {/* 写真から探すの行を削除しました */}
         <li><Link to="/about"><div className="icon"><AiOutlineAppstore /></div><div className="text">マップについて</div></Link></li>
