add6f0b5d37cab4cef8f62b75a91e068f78e43b7
airi <airi.hisa@icloud.com>
2026-09-07 22:26:01 +0900

    Update Tabbar.tsx

--- src/App/Tabbar.tsx
+++ src/App/Tabbar.tsx
@@ -2,19 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 import './Tabbar.scss'
 
-// FaCamera を削除しました
-import { FaList, FaHome,} from "react-icons/fa"
+// 確実に存在する FaMap に変更
+import { FaList, FaMap } from "react-icons/fa"
 import { AiOutlineAppstore } from "react-icons/ai"
 
 const Content = () => {
   return (
     <div className="tabbar">
       <ul>
-        <li><Link to="/"><div className="icon"><FaMap /></div><div className="text">マップ</div></Link></li>
-        <li><Link to="/list"><div className="icon"><FaList /></div><div className="text">リスト</div></Link></li>
-        {/* カテゴリの行を削除しました */}
-        {/* 写真から探すの行を削除しました */}
-        <li><Link to="/about"><div className="icon"><AiOutlineAppstore /></div><div className="text">マップについて</div></Link></li>
+        <li><Link to="/"><div className="icon"><FaMap /></div><div className="text">Map</div></Link></li>
+        <li><Link to="/list"><div className="icon"><FaList /></div><div className="text">List</div></Link></li>
+        <li><Link to="/about"><div className="icon"><AiOutlineAppstore /></div><div className="text">About</div></Link></li>
       </ul>
     </div>
   );
